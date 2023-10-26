8a31dae0d7e3accf984ef57982309d8ff02371a9
Kishore .U <nagakishore64@gmail.com>
2023-10-26 07:49:48 +0530

    switchcase

---
+++ swithcase.js
@@ -0,0 +1,17 @@
+//switch-case
+var light="green"
+switch(light) {
+   case 'green':
+       console.log("Drive");
+       break;
+   case 'orange':
+       console.log("Get ready");
+       break;
+   case 'red':
+       console.log("Don't drive");
+       break;
+   default:
+       //this block will run if no condition matches
+       console.log('The light is not green, orange, or red');
+       break;
+}
