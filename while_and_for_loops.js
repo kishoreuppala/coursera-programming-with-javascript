5d0dff94cf598b7d1d89479f3422bed7785e7ef3
Kishore .U <nagakishore64@gmail.com>
2023-10-28 07:55:43 +0530

    while and for loops

---
+++ while_and_for_loops.js
@@ -0,0 +1,79 @@
+//example 1
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+
+console.log("\nFor loop example:");
+for (var i=1;i<6;i++)
+{
+    console.log(i);
+}
+console.log('Counting completed!');
+
+//2
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+console.log("\nFor loop example:");
+for (let j=5;j>1;j--)
+{
+    console.log(j);
+}
+console.log('Countdown finished!')
+
+//3
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+
+console.log('\n While loop example:');
+var i=1;
+while (i < 6 )
+{
+    console.log(i);
+    i=i+1;
+}
+console.log('Counting completed!')
+
+//4
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+console.log("\n While loop example:");
+var i = 5;
+while (i>0)
+{
+    console.log(i);
+    i=i-1;
+}
+console.log('Countdown finished!')
+
+//5
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+
+var i=2018;
+console.log("\nWhile loop example:");
+while (i<2023)
+{
+    console.log(i);
+    i=i+1;
+}
+
