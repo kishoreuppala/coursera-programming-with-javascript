cbf5f65407b6df6decf94cde867afd1c1ace90bc
Kishore .U <nagakishore64@gmail.com>
2023-10-31 23:42:18 +0530

    nested_loops

---
+++ nested_loops.js
@@ -0,0 +1,6 @@
+//nested loops - one inside another
+for (var firstNum = 0; firstNum < 2; firstNum++) {
+    for (var secondNum = 0; secondNum < 5; secondNum++) {
+        console.log("First Number: ",firstNum + ", " + "Second Number: ",secondNum);
+    }
+}
