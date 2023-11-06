61764cb282c8eb06c01a760fc73f522cd7d54d7f
Kishore .U <nagakishore64@gmail.com>
2023-11-06 22:30:48 +0530

    Object Literals and the Brackets Notation

--- objects.js
+++ objects.js
@@ -38,3 +38,38 @@ console.log(house); // {rooms: 3, color: "brown", priceUSD: 10000, windows: 10}
 
 house.windows = 11;
 console.log(house); // {rooms: 3, color: "brown", priceUSD: 10000, windows: 11}
+
+
+//Object literals and the brackets notation
+
+var house2 = {};
+house2["rooms"] = 4;
+house2['color']= "pink";
+house2["priceUSD"] = 12345;
+console.log(house2); // {rooms: 4, color: 'pink', priceUSD: 12345}
+
+var car = {};
+car.color = "red";
+car["color"] = "green";
+car["speed"] = 200;
+car.speed = 100;
+console.log(car); // {color: "green", speed: 100}
+
+car["number of doors"] = 4;
+console.log(car); // {color: 'green', speed: 100, number of doors: 4}
+
+car["2022"] = 1901;
+console.log(car); // {2022: 1901, color: 'green', speed: 100, number of doors: 4}
+
+var arrOfKeys = ['speed', 'altitude', 'color'];
+var drone = {
+    speed: 100,
+    altitude: 200,
+    color: "red"
+}
+for (var i = 0; i < arrOfKeys.length; i++) {
+    console.log(drone[arrOfKeys[i]])
+}
+
+
+
