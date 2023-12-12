ee8eb61bb6b51e31cd742311f9be3e536a303480
Kishore .U <nagakishore64@gmail.com>
2023-12-12 23:43:03 +0530

    built-in object methods & loop over any objects own property keys and values

---
+++ Built-in object methods.js
@@ -0,0 +1,20 @@
+//object.keys() method
+const car2 = {
+    speed: 2000,
+    color: "blue"
+}
+console.log(Object.keys(car2)); // ['speed','color']
+
+//Object.values() method
+const car3 = {
+    speed: 2000,
+    color: "blue"
+}
+console.log(Object.values(car3)); // ['speed','color']
+
+//Object.entries() method
+const car4 = {
+    speed: 4000,
+    color: 'orange'
+}
+console.log(Object.entries(car4));
---
+++ loop over any object's own property keys and values.js
@@ -0,0 +1,28 @@
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
+}
+
+for( const key of Object.keys(clothingItem) ) {
+    console.log(key, ":", clothingItem[key])
+}
+
+//loop over object's own property keys and values
+function testBracketsDynamicAccess() {
+  var dynamicKey;
+  if(Math.random() > 0.5) {
+    dynamicKey = "speed";
+   }else{
+     dynamicKey = "color";
+   }
+
+    var drone = {
+      speed: 15,
+      color: "orange"
+    }
+
+    console.log(drone[dynamicKey]);
+}
+testBracketsDynamicAccess();
