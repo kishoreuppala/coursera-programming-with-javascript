59d4d4e99cba725876e1cbb92c77ae113c2ea017
Kishore .U <nagakishore64@gmail.com>
2024-02-11 23:03:09 +0530

    Added .gitignore, jest test demo example

---
+++ jest test demo.js
@@ -0,0 +1,5 @@
+//Pre-requisites
+//Check node and npm are installed: node --version;npm --version;
+//Check jest is installed: jest --version
+//Before installing jest, npm init -y ( to add a package.json file)
+//npn install --save-dev jest (to install jest for this project instead of globally on the machine)
